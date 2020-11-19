cdc834b0edd284df403aaedcdd699b679e0d6371
Michael Zhou <mz25@illinois.edu>
2020-11-19 04:23:09 -0600

    add password screen

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./styles.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
@@ -9,46 +9,60 @@ import FrontPage from "../components/front_page";
 import NavBar from "../components/nav_bar";
 
 const App = () => {
+  const pass = "placeholder";
+  const [locked, setLocked] = useState(true);
   const nodeRefFrontPage = useRef(null);
   const nodeRefStock = useRef(null);
   return (
     <Router>
-      <div className="app">
-        <div className="app-background" />
-        <div className="app-navbar">
-          <NavBar />
+      {locked ? (
+        <div className="app-locked">
+          <input
+            className="app-locked-text"
+            type="password"
+            onChange={(e) => {
+              if (e.target.value === pass) setLocked(false);
+            }}
+          ></input>
         </div>
-        <div className="app-content">
-          <Route exact path="/">
-            {({ match }) => (
-              <CSSTransition
-                nodeRef={nodeRefFrontPage}
-                in={match != null}
-                timeout={1000}
-                classNames="page"
-                unmountOnExit
-              >
-                <FrontPage ref={nodeRefFrontPage} />
-              </CSSTransition>
-            )}
-          </Route>
+      ) : (
+        <div className="app">
+          <div className="app-background" />
+          <div className="app-navbar">
+            <NavBar />
+          </div>
+          <div className="app-content">
+            <Route exact path="/">
+              {({ match }) => (
+                <CSSTransition
+                  nodeRef={nodeRefFrontPage}
+                  in={match != null}
+                  timeout={1000}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <FrontPage ref={nodeRefFrontPage} />
+                </CSSTransition>
+              )}
+            </Route>
+          </div>
+          <div className="app-content">
+            <Route path="/:ticker">
+              {({ match }) => (
+                <CSSTransition
+                  nodeRef={nodeRefStock}
+                  in={match != null}
+                  timeout={1000}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <Stock ref={nodeRefStock} />
+                </CSSTransition>
+              )}
+            </Route>
+          </div>
         </div>
-        <div className="app-content">
-          <Route path="/:ticker">
-            {({ match }) => (
-              <CSSTransition
-                nodeRef={nodeRefStock}
-                in={match != null}
-                timeout={1000}
-                classNames="page"
-                unmountOnExit
-              >
-                <Stock ref={nodeRefStock} />
-              </CSSTransition>
-            )}
-          </Route>
-        </div>
-      </div>
+      )}
     </Router>
   );
 };
