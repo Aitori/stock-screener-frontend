b96630a06d8fbfa612038588c9b30104eaf5f5d8
Michael Zhou <mz25@illinois.edu>
2020-11-25 22:55:52 -0600

    some packages

--- src/components/news_card/index.js
+++ src/components/news_card/index.js
@@ -3,15 +3,10 @@ import "./styles.scss";
 
 const NewsCard = (props) => {
   return (
-      <a
-        href={props.url}
-        className="news-card"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="news-card-title">{props.title}</div>
-        <div className="news-card-summary">{props.summary}</div>
-      </a>
+    <a href={props.url} className="news-card" target="_blank" rel="noreferrer">
+      <div className="news-card-title">{props.title}</div>
+      <div className="news-card-summary">{props.summary}</div>
+    </a>
   );
 };
 
