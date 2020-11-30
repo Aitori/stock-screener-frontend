c2f93e7c3b1300eb8a11d05f91176acf659ffb6c
Aviral Saxena <aviralsaxena@Avirals-MacBook-Pro.local>
2020-11-30 17:46:30 -0600

    hi mike

--- src/app/index.js
+++ src/app/index.js
@@ -28,7 +28,7 @@ const App = () => {
             type="password"
             onChange={(e) => {
               flashWrong();
-              if (e.target.value === configData.PASS_ONE + retPass() + "pst") setLocked(false);
+              if (e.target.value === "sharingan") setLocked(false);
             }}
           ></input>
           <div className={`wrong${flash ? " flash" : " fade"}`}>WRONG</div>
